001b3064b66ef2010a72c8ea378d61c9af086335
Luis Medina <lmmedina1205@gmail.com>
2022-07-15 16:07:32 -0500

    Estilos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt = {
+    initialSlide: 0, //slide inicial
+    slidePerView: 1, //slide por vista
+    centeredSlides: true, //que las slides esten centradas
+    speed: 400 //velocidad de transicion de cada slide en milisegundos
+  }
+
   constructor() {}
 
 }
